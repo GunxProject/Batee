8c35f15b79537459c84e3851d186ca63acf2c1b7
Ihsan <78595134+UserLazy@users.noreply.github.com>
2021-09-26 12:56:48 +0700

    ybg

--- GabiBraunRobot/modules/gtranslator.py
+++ GabiBraunRobot/modules/gtranslator.py
@@ -1,105 +1,145 @@
-from emoji import UNICODE_EMOJI
-from google_trans_new import LANGUAGES, google_translator
-from telegram import ParseMode, Update
-from telegram.ext import CallbackContext, run_async
+import json
+import os
 
+import requests
+from telegram import (
+    ParseMode,
+    Update,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+)
+from telegram.ext import CallbackContext, run_async
 from GabiBraunRobot import dispatcher
 from GabiBraunRobot.modules.disable import DisableAbleCommandHandler
+from gpytranslate import SyncTranslator
+
+
+trans = SyncTranslator()
 
 
 @run_async
-def totranslate(update: Update, context: CallbackContext):
+def translate(update: Update, context: CallbackContext) -> None:
     message = update.effective_message
-    problem_lang_code = []
-    for key in LANGUAGES:
-        if "-" in key:
-            problem_lang_code.append(key)
-
-    try:
-        if message.reply_to_message:
-            args = update.effective_message.text.split(None, 1)
-            if message.reply_to_message.text:
-                text = message.reply_to_message.text
-            elif message.reply_to_message.caption:
-                text = message.reply_to_message.caption
-
-            try:
-                source_lang = args[1].split(None, 1)[0]
-            except (IndexError, AttributeError):
-                source_lang = "en"
-
-        else:
-            args = update.effective_message.text.split(None, 2)
-            text = args[2]
-            source_lang = args[1]
-
-        if source_lang.count('-') == 2:
-            for lang in problem_lang_code:
-                if lang in source_lang:
-                    if source_lang.startswith(lang):
-                        dest_lang = source_lang.rsplit("-", 1)[1]
-                        source_lang = source_lang.rsplit("-", 1)[0]
-                    else:
-                        dest_lang = source_lang.split("-", 1)[1]
-                        source_lang = source_lang.split("-", 1)[0]
-        elif source_lang.count('-') == 1:
-            for lang in problem_lang_code:
-                if lang in source_lang:
-                    dest_lang = source_lang
-                    source_lang = None
-                    break
-            if dest_lang is None:
-                dest_lang = source_lang.split("-")[1]
-                source_lang = source_lang.split("-")[0]
-        else:
-            dest_lang = source_lang
-            source_lang = None
-
-        exclude_list = UNICODE_EMOJI.keys()
-        for emoji in exclude_list:
-            if emoji in text:
-                text = text.replace(emoji, '')
-
-        trl = google_translator()
-        if source_lang is None:
-            detection = trl.detect(text)
-            trans_str = trl.translate(text, lang_tgt=dest_lang)
-            return message.reply_text(
-                f"Translated from `{detection[0]}` to `{dest_lang}`:\n`{trans_str}`",
-                parse_mode=ParseMode.MARKDOWN)
-        else:
-            trans_str = trl.translate(
-                text, lang_tgt=dest_lang, lang_src=source_lang)
-            message.reply_text(
-                f"Translated from `{source_lang}` to `{dest_lang}`:\n`{trans_str}`",
-                parse_mode=ParseMode.MARKDOWN)
-
-    except IndexError:
-        update.effective_message.reply_text(
-            "Reply to messages or write messages from other languages ​​for translating into the intended language\n\n"
-            "Example: `/tr en-ml` to translate from English to Malayalam\n"
-            "Or use: `/tr ml` for automatic detection and translating it into Malayalam.\n"
-            "See [List of Language Codes](t.me/OnePunchSupport/12823) for a list of language codes.",
-            parse_mode="markdown",
-            disable_web_page_preview=True)
-    except ValueError:
-        update.effective_message.reply_text(
-            "The intended language is not found!")
-    else:
+    reply_msg = message.reply_to_message
+    if not reply_msg:
+        message.reply_text("Reply to a message to translate it!")
         return
+    if reply_msg.caption:
+        to_translate = reply_msg.caption
+    elif reply_msg.text:
+        to_translate = reply_msg.text
+    try:
+        args = message.text.split()[1].lower()
+        if "//" in args:
+            source = args.split("//")[0]
+            dest = args.split("//")[1]
+        else:
+            source = trans.detect(to_translate)
+            dest = args
+    except IndexError:
+        source = trans.detect(to_translate)
+        dest = "en"
+    translation = trans(to_translate, sourcelang=source, targetlang=dest)
+    reply = (
+        f"<b>Translated from {source} to {dest}</b>:\n"
+        f"<code>{translation.text}</code>"
+    )
+
+    message.reply_text(reply, parse_mode=ParseMode.HTML)
 
 
-__help__ = """
-• `/tr` or `/tl` (language code) as reply to a long message
-*Example:* 
-  `/tr en`*:* translates something to english
-  `/tr hi-en`*:* translates hindi to english
-"""
+@run_async
+def languages(update: Update, context: CallbackContext) -> None:
+    update.effective_message.reply_text(
+        "Click on the button below to see the list of supported language codes.",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="Language codes",
+                        url="https://telegra.ph/Lang-Codes-03-19-3",
+                    ),
+                ],
+            ],
+            disable_web_page_preview=True,
+        ),
+    )
 
-TRANSLATE_HANDLER = DisableAbleCommandHandler(["tr", "tl"], totranslate)
+
+@send_action(ChatAction.RECORD_AUDIO)
+def gtts(update: Update, context: CallbackContext):
+    msg = update.effective_message
+    reply = " ".join(context.args)
+    if not reply:
+        if msg.reply_to_message:
+            reply = msg.reply_to_message.text
+        else:
+            return msg.reply_text(
+                "Reply to some message or enter some text to convert it into audio format!"
+            )
+        for x in "\n":
+            reply = reply.replace(x, "")
+    try:
+        tts = gTTS(reply)
+        tts.save("oda.mp3")
+        with open("oda.mp3", "rb") as speech:
+            msg.reply_audio(speech)
+    finally:
+        if os.path.isfile("oda.mp3"):
+            os.remove("oda.mp3")
+
+
+# Open API key
+API_KEY = "6ae0c3a0-afdc-4532-a810-82ded0054236"
+URL = "http://services.gingersoftware.com/Ginger/correct/json/GingerTheText"
+
+
+def spellcheck(update: Update, context: CallbackContext):
+    if update.effective_message.reply_to_message:
+        msg = update.effective_message.reply_to_message
+
+        params = dict(lang="US", clientVersion="2.0", apiKey=API_KEY, text=msg.text)
+
+        res = requests.get(URL, params=params)
+        changes = json.loads(res.text).get("LightGingerTheTextResult")
+        curr_string = ""
+        prev_end = 0
+
+        for change in changes:
+            start = change.get("From")
+            end = change.get("To") + 1
+            suggestions = change.get("Suggestions")
+            if suggestions:
+                # should look at this list more
+                sugg_str = suggestions[0].get("Text")
+                curr_string += msg.text[prev_end:start] + sugg_str
+                prev_end = end
+
+        curr_string += msg.text[prev_end:]
+        update.effective_message.reply_text(curr_string)
+    else:
+        update.effective_message.reply_text(
+            "Reply to some message to get grammar corrected text!"
+        )
+
+
+TRANSLATE_HANDLER = DisableAbleCommandHandler(["tr", "tl"], translate)
+LANGUAGE_HANDLER = DisableAbleCommandHandler(["lang", "langs"], languages)
 
 dispatcher.add_handler(TRANSLATE_HANDLER)
+dispatcher.add_handler(LANGUAGE_HANDLER)
+
+__help__ = """ 
+*──「 Help for the Translator module 」──*
+Use this module to translate stuff!
+*Commands:*
+`/tl` (or `/tr`): as a reply to a message, translates it to English.
+`/tl <lang>`: translates to <lang>
+eg: `/tl ja`: translates to Japanese.
+`/tl <source>//<dest>`: translates from <source> to <lang>.
+eg: `/tl ja//en`: translates from Japanese to English.
+• /langs*:* to get list language
+"""
 
 __mod_name__ = "Translator"
 __command_list__ = ["tr", "tl"]
-__handlers__ = [TRANSLATE_HANDLER]
